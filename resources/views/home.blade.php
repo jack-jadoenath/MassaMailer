c85029c8df197809b05999ae1158ff6fe768e7e9
Micha Godefroij <michagodefroy@gmail.com>
2019-06-03 13:11:15 +0200

    Laravel Installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.master')
+
+@section('content')
+
+<div class="row"> 
+    <div class="col-md-6" >
+        <h1>Massa Mailer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius a libero vel mollis. Curabitur fermentum massa nunc, et tempor sem efficitur nec. Vivamus eleifend arcu ac ante convallis tristique. Vestibulum vel sapien sed tellus vulputate elementum. Vivamus nulla nibh, fringilla sagittis leo scelerisque, lobortis aliquam enim. Integer tincidunt scelerisque lorem sed mattis. Etiam convallis consequat mauris, ultrices aliquet tortor maximus sed. Quisque lacinia ex quis luctus lobortis. Sed dictum id purus pretium tincidunt. Ut fringilla, tellus et dignissim lobortis, massa orci eleifend justo, id porta nulla massa nec enim. Suspendisse vitae risus lacinia, iaculis augue vel, ultrices sem. Vestibulum non scelerisque sem. Pellentesque eget venenatis enim. Pellentesque pulvinar finibus hendrerit. Suspendisse potenti. Morbi eu lorem vehicula, vehicula purus nec, euismod ante.</p>
+    </div>
+
+    <div class="col-md-6">
+        <img class="feature-image" src="{{asset('img/mail-desk.png')}}" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <img class="feature-image" src="{{asset('img/notegit.png')}}" />
+    </div>
+
+    <div class="col-md-6" >
+        <h1>Massa Mailer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius a libero vel mollis. Curabitur fermentum massa nunc, et tempor sem efficitur nec. Vivamus eleifend arcu ac ante convallis tristique. Vestibulum vel sapien sed tellus vulputate elementum. Vivamus nulla nibh, fringilla sagittis leo scelerisque, lobortis aliquam enim. Integer tincidunt scelerisque lorem sed mattis. Etiam convallis consequat mauris, ultrices aliquet tortor maximus sed. Quisque lacinia ex quis luctus lobortis. Sed dictum id purus pretium tincidunt. Ut fringilla, tellus et dignissim lobortis, massa orci eleifend justo, id porta nulla massa nec enim. Suspendisse vitae risus lacinia, iaculis augue vel, ultrices sem. Vestibulum non scelerisque sem. Pellentesque eget venenatis enim. Pellentesque pulvinar finibus hendrerit. Suspendisse potenti. Morbi eu lorem vehicula, vehicula purus nec, euismod ante.</p>
+    </div>
+</div>
+
+@endsection
