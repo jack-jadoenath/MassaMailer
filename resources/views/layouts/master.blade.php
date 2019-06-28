7869f39a3e68660a7d8af30b59a4ede0c47b5251
Reeeeemco <remco.geerdink@gmail.com>
2019-06-28 14:41:03 +0200

    hot fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,7 @@
         <nav class="navbar navbar-expand-sm navbar">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('MassaMailer') }}
+                    MassaMailer
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
