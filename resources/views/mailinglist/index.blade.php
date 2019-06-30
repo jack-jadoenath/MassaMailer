35cad6d84dd7ef2d8cebf97e6ad3f3c18a68731b
Jack Jadoenath <sjadoenath69@gmail.com>
2019-06-30 14:43:05 +0200

    maar waarom ziet ie ID niet?

--- resources/views/mailinglist/index.blade.php
+++ resources/views/mailinglist/index.blade.php
@@ -5,15 +5,18 @@
 <div class="md-7">
     <h1 class="mt-5">Mailing list</h1>
 
-    <table class="table .tablestriped">
+    <table class="table">
         <thead class="thead">
             <tr>
-                <th scope="col">Name</th>
+                <th scope="col">ID</th>
+                <th scope="col">Naam</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                
+                <td scope="row">{{ $mailinglists->id }}</td>
+                <td> {{ $mailinglists->name }} </td>
+
             </tr>
         </tbody>
     </table>
